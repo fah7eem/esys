10b3554337b63ff103a5d71434ca354cee373389
fah7eem <fseedat7@gmail.com>
2016-09-26 21:09:49 +0200

    added color

--- index.php
+++ index.php
@@ -3,10 +3,11 @@ include ('functions.php');
 echo "<h1>Expert System</h1>";
 
 if($_POST['subindex'] === 'OK' and $_FILES['file'])
-{
+{	echo "<div id='ans' >";
 	$target = file($_FILES['file']['tmp_name']);
 	foreach ($target as $line)
 		read_line ($line);
+	echo "</div>";
 }
 ?>
 <HTML>
@@ -18,13 +19,22 @@ if($_POST['subindex'] === 'OK' and $_FILES['file'])
 	display: table;
 	margin: auto;
 	background-color: #aa80ff;
+	font-size: 150%;
+	}
+	#ans 
+	{
+	background-color: #990099;
+	color: white;
+	text-align: center;
+	box-shadow: 10px 10px 5px #888888;
+	padding: 15px;
 	}
 </style>
 </HEAD>
 <BODY>
 <form name='fileup' action='index.php' method='POST' enctype="multipart/form-data">
 <input for='file' type='file' name='file'/>
-<br><br><input type='submit' name='subindex' value='OK' />
+<br><input type='submit' name='subindex' value='OK' />
 <input type='submit' value='Refresh' />
 </form>
 </BODY>
