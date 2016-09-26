f5e62bc5c5bd7ce06195b4c560011c7408bc1877
fseedat <fseedat@e5r1p1.jnb.42.fr>
2016-09-26 14:49:35 +0200

    removed spaces

--- functions.php
+++ functions.php
@@ -5,9 +5,17 @@ function read_line($line)
 	if(preg_match('/^(.*?)#/',$line, $ret))
 	{
 		$ret = preg_replace("/#/" , "\0" , $ret);
+		$ret[0] = str_replace(' ', '', $ret[0]);
 		echo $ret[0]."<BR>";
 	}
 	else
+	{
+		$line = str_replace(' ', '' , $line);
 		echo $line."<BR>";
+	}
+}
+
+function operater($line)
+{
 }
 ?>
