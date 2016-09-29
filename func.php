d38fbf2ee68123a1a6bf8784e3a0372d8ac8027a
fah7eem <fseedat7@gmail.com>
2016-09-29 10:37:55 +0200

    added func.php and preg_match for ()

---
+++ func.php
@@ -0,0 +1,59 @@
+<?PHP
+
+function	and_($line)
+{
+	$i = 0;
+	while($line[$i])
+	{
+		if($line[$i] === '+')
+		{
+			$a =	$line[$i - 1];
+			$b =	$line[$i + 1];
+			if($$a === 1 and $$b === 1)
+				return 1;
+			else
+				return 0;
+		}
+		$i++;
+	}	
+
+}
+
+function	or_($line)
+{
+	$i = 0;
+	while($line[$i])
+	{
+		if($line[$i] === '|')
+		{
+			$a =    $line[$i - 1];
+			$b =    $line[$i + 1];
+			if($$a === 1 or $$b === 1)
+				return 1;
+			else
+				return 0;
+		}
+		$i++;
+	}
+
+}
+
+function	xor_($line)
+{
+	$i = 0;
+	while($line[$i])
+	{
+		if($line[$i] === '^')
+		{
+			$a =    $line[$i - 1];
+			$b =    $line[$i + 1];
+			if($$a === 1 xor $$b === 1)
+				return 1;
+			else
+				return 0;
+		}
+		$i++;
+	}
+
+}
+?>
--- run.class.php
+++ run.class.php
@@ -4,9 +4,9 @@ include_once('expert.class.php');
 class run extends expert
 {
 	public $alpha = array('A' => 0 , 'B' => 0 , 'C' => 0 , 'D' => 0 , 'E' => 0 , 'F' => 0,
-		'G' => 0 , 'H' => 0 , 'I' => 0 , 'J' => 0 , 'K' => 0 , 'L' => 0 , 'M' => 0 , 'N' => 0 , 'O' => 0,
-		'P' => 0 , 'Q' => 0 , 'R' => 0 , 'S' => 0 , 'T' => 0 , 'U' => 0 , 'V' => 0 , 'W' => 0 , 'X' => 0,
-		'Y' => 0 , 'Z' => 0);
+		'G' => 0 , 'H' => 0 , 'I' => 0 , 'J' => 0 , 'K' => 0 , 'L' => 0 , 'M' => 0 , 
+		'N' => 0 , 'O' => 0,'P' => 0 , 'Q' => 0 , 'R' => 0 , 'S' => 0 , 'T' => 0 , 
+		'U' => 0 , 'V' => 0 , 'W' => 0 , 'X' => 0, 'Y' => 0 , 'Z' => 0);
 
 	public function __construct($file)
 	{
@@ -24,7 +24,11 @@ class run extends expert
 
 	public function test()
 	{
-		$this->print_alpha();
+		foreach($this->rules as $line)
+		{
+		if(preg_match_all('/\((.*?)\)/', $line, $matches))
+			print_r ($matches[1]);
+		}
 	}
 
 	private function assign()
