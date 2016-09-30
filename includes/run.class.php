feafe3418821282e292df34632c12c12150a9831
fah7eem <fseedat7@gmail.com>
2016-09-30 22:54:52 +0200

    dleted functions, added switch_ function to run class & created  array

--- includes/run.class.php
+++ includes/run.class.php
@@ -1,13 +1,12 @@
 <?PHP
 include_once('expert.class.php');
-include_once('func.php');
 
 class run extends expert
 {
 	public $alpha = array('A' => 0 , 'B' => 0 , 'C' => 0 , 'D' => 0 , 'E' => 0 , 'F' => 0,
-		'G' => 0 , 'H' => 0 , 'I' => 0 , 'J' => 0 , 'K' => 0 , 'L' => 0 , 'M' => 0 , 
-		'N' => 0 , 'O' => 0,'P' => 0 , 'Q' => 0 , 'R' => 0 , 'S' => 0 , 'T' => 0 , 
-		'U' => 0 , 'V' => 0 , 'W' => 0 , 'X' => 0, 'Y' => 0 , 'Z' => 0);
+			'G' => 0 , 'H' => 0 , 'I' => 0 , 'J' => 0 , 'K' => 0 , 'L' => 0 , 'M' => 0 , 
+			'N' => 0 , 'O' => 0,'P' => 0 , 'Q' => 0 , 'R' => 0 , 'S' => 0 , 'T' => 0 , 
+			'U' => 0 , 'V' => 0 , 'W' => 0 , 'X' => 0, 'Y' => 0 , 'Z' => 0);
 
 	public function __construct($file)
 	{
@@ -24,12 +23,31 @@ class run extends expert
 	}
 
 	public function test()
-	{
-		foreach($this->rules as $line)
+	{	
+		$left = array();
+		foreach($this->rules as $rule)
 		{
-		if(preg_match_all('/\((.*?)\)/', $line, $matches))
-			print_r ($matches[1]);
+			$i = 0;
+			$split = explode("=", $rule);
+			$line = $split[0];
+			if(!$line[1])
+				array_push($left,$this->alpha[$line[0]]);
+			else
+			{	
+				while($line[$i])
+				{
+					if($line[$i] === '+' || $line[$i] === '|' || $line === '^')
+					{
+						$a = $this->alpha[$line[$i - 1]];
+						$o = $line[$i];
+						$b = $this->alpha[$line[$i + 1]];
+					array_push($left, $this->switch_($a, $o , $b));
+					}	
+					$i++;
+				}
+			}
 		}
+		print_r ($left);
 	}
 
 	private function assign()
@@ -42,6 +60,25 @@ class run extends expert
 				$this->alpha[$c] = 1;
 			$i++;
 		}	
+	}
+
+	protected function switch_($value1, $operator, $value2)
+	{
+		switch ($operator) 
+		{
+			case '+':
+				return $value1 & $value2;
+				break;
+			case '|':
+				return $value1 | $value2;
+				break;
+			case '^':
+				return $value1 ^ $value2;
+				break;
+			default:
+				return false;
+		}
+		return false;
 	}
 }
 ?>
