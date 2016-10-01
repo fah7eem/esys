0fc88de464e1df23b988115ae824b65f28d7c5bd
fah7eem <fseedat7@gmail.com>
2016-10-01 12:28:11 +0200

    can solve simple sum, recur_ and change_ functions are new

--- ex/ex1.es
+++ ex/ex1.es
@@ -4,9 +4,7 @@ G + H => F
 I + J => G
 G => H
 L + M => K
-O + P => L + N
 N => M
 
-#=DEIJOP # AFKP is true
 =DEIJP # AFP is true, K is false
 ?AFKP
--- includes/run.class.php
+++ includes/run.class.php
@@ -7,6 +7,8 @@ class run extends expert
 			'G' => 0 , 'H' => 0 , 'I' => 0 , 'J' => 0 , 'K' => 0 , 'L' => 0 , 'M' => 0 , 
 			'N' => 0 , 'O' => 0,'P' => 0 , 'Q' => 0 , 'R' => 0 , 'S' => 0 , 'T' => 0 , 
 			'U' => 0 , 'V' => 0 , 'W' => 0 , 'X' => 0, 'Y' => 0 , 'Z' => 0);
+	public $left = array();
+	public $right = array();
 
 	public function __construct($file)
 	{
@@ -21,17 +23,62 @@ class run extends expert
 			echo $elem.":".$line."<br>";
 		}
 	}
-
+	
+	public function display()
+	{
+		$i = 0;	
+		while($this->query[$i])
+		{
+			$k = $this->query[$i];
+			if(ctype_alpha($k))
+			{
+			echo $k." is ";
+			if($this->alpha[$k] === 1)
+				echo "True";
+			else
+				echo "False";
+			echo "<BR>";
+			}
+		$i++;
+		}	
+	} 
+	
 	public function test()
+	{
+		$this->recur_();
+		$this->display();
+	}
+
+	private function recur_()
+	{
+		$this->change_();
+		$chk = array();
+		$chk = array_combine($this->right , $this->left);	
+		 foreach($chk as $lft => $rht)
+		{
+			$a = $rht;
+			$b = $lft;
+			if($a != $this->alpha[$b])
+			{
+				$this->alpha[$b] = $a;
+				$this->recur_();
+			}
+		
+		}
+	}
+
+	private function change_()
 	{	
-		$left = array();
+	$this->left = array();
+	$this->right = array();	
 		foreach($this->rules as $rule)
 		{
 			$i = 0;
 			$split = explode("=", $rule);
 			$line = $split[0];
+			array_push($this->right, $split[1]);
 			if(!$line[1])
-				array_push($left,$this->alpha[$line[0]]);
+				array_push($this->left,$this->alpha[$line[0]]);
 			else
 			{	
 				while($line[$i])
@@ -41,13 +88,12 @@ class run extends expert
 						$a = $this->alpha[$line[$i - 1]];
 						$o = $line[$i];
 						$b = $this->alpha[$line[$i + 1]];
-					array_push($left, $this->switch_($a, $o , $b));
+					array_push($this->left, $this->switch_($a, $o , $b));
 					}	
 					$i++;
 				}
 			}
 		}
-		print_r ($left);
 	}
 
 	private function assign()
